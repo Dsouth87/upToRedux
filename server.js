aeebbe18902fac28ea243af37198891c4673e286
derek southard <derek.southard@airbnb.com>
2020-01-06 12:58:00 -0800

    added basic routes

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 8888
 
 connectDB()
 
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/user', require('./routes/api/user'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
