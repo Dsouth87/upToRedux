ff0c579c0a6a3e769ce7d70b75aacde33c462aaa
derek southard <derek.southard@airbnb.com>
2020-03-23 13:16:11 -0700

    prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
+const path = require('path')
 
 const PORT = process.env.PORT || 8888
 
@@ -12,6 +13,17 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/user', require('./routes/api/user'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+//Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
